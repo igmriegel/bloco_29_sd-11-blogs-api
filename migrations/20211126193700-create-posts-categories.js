cb95e8f6e018ad84d7f958e41d32df3387389a2a
Igor Marcos Riegel <igor.riegel@gmail.com>
2021-11-26 17:48:18 -0300

    concluida a modelagem das migrations

---
+++ migrations/20211126193700-create-posts-categories.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('PostsCategories', {
+      postId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'BlogPosts',
+          key: 'id',
+        },
+        onDelete: 'cascade',
+        primaryKey: true,
+      },
+      categoryId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Categories',
+          key: 'id',
+        },
+        onDelete: 'cascade',
+        primaryKey: true,
+      }
+    },{
+      timestamps: false,
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('PostsCategories');
+  }
+};
