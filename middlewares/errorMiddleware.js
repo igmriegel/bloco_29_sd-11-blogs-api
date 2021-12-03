04ba35937da91896e650e040e900f10df607ec35
Igor Marcos Riegel <igor.riegel@gmail.com>
2021-12-03 17:04:43 -0300

    implementadas mudanças para tratar erro duplicidade sequelize

--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -1,15 +1,26 @@
 const errorMiddleware = (err, _req, res, _next) => {
-  const { typeOfError, message, httpCode } = err;
+  const {
+    typeOfError,
+    message,
+    httpCode,
+    name,
+   } = err;
 
-  if (typeOfError !== 'Internal Validation Error') {
-    console.log(`Server internal error: ${err}`);
+  if (name === 'SequelizeUniqueConstraintError') {
+    const { original: sqlMessage } = err;
+    const userMessage = 'User already registered';
 
-    return res.status(500).json(message);
+    console.log(`(ERROR-LOG) - Sequelize error unique const: ${sqlMessage}`);
+    return res.status(409).json({ message: userMessage });
   }
 
-  console.log(`Error in validation rules: ${message}`);
+  if (typeOfError === 'Internal Validation Error') {
+    console.log(`(ERROR-LOG) - Validation rules error: ${message}`);
+    return res.status(httpCode).json({ message });
+  }
 
-  return res.status(httpCode).json(message);
+  console.log(`(ERROR-LOG) - Internal server error: ${err}`);
+  return res.status(500).json(err);
 };
 
 module.exports = errorMiddleware;
