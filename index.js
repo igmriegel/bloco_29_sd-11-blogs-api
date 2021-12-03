364e9927a030dacf0a4873a61f90726f7c7a84c4
Igor Marcos Riegel <igor.riegel@gmail.com>
2021-12-03 17:06:29 -0300

    implementada a utilização do middleware de erro e implementada rota user

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
 
+const controllers = require('./controllers');
+const errorMiddleware = require('./middlewares/errorMiddleware');
+
 const app = express();
 
-app.listen(3000, () => console.log('ouvindo porta 3000!'));
+app.use(express.json());
+
+app.use('/user', controllers.userController);
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
-  response.send('Let\'s go!!');
+  response.send();
 });
+
+app.use(errorMiddleware);
+
+app.listen(3000, () => console.log('ouvindo porta 3000!'));
