ec668690ac250248e3a2662c2448011cc10d4a97
Igor Marcos Riegel <igor.riegel@gmail.com>
2021-12-04 03:43:37 -0300

    alterada a importação da camada services

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
-const { createUser } = require('../services/userServices');
+const { userServices } = require('../services');
 
 router.post('/', async (req, res, next) => {
   try {
     const { displayName, email, password, image } = req.body;
-    const newUser = await createUser({ displayName, email, password, image });
+    const newUser = await userServices.createUser({ displayName, email, password, image });
 
     return res.status(201).json({ token: newUser });
   } catch (e) {
