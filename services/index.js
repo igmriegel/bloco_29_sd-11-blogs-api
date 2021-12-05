7de2297cd198484da0022c7db5f1a3568437fb55
Igor Marcos Riegel <igor.riegel@gmail.com>
2021-12-05 19:27:03 -0300

    adicionados os services de cateogry para o index de services

--- services/index.js
+++ services/index.js
@@ -1,7 +1,9 @@
 const userServices = require('./userServices');
 const loginServices = require('./loginServices');
+const categoryServices = require('./categoryServices');
 
 module.exports = {
   userServices,
   loginServices,
+  categoryServices,
 };
