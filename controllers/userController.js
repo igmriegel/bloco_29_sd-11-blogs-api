6e237e3f3093be5c9a8c2885e3865be782ff276b
Igor Marcos Riegel <igor.riegel@gmail.com>
2021-12-05 18:21:38 -0300

    Implementado controller para retornar todos users.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 const { userServices } = require('../services');
+const { User } = require('../models');
+const authMiddleware = require('../middlewares/authMiddleware');
 
 router.post('/', async (req, res, next) => {
   try {
@@ -7,6 +9,16 @@ router.post('/', async (req, res, next) => {
     const newUser = await userServices.createUser({ displayName, email, password, image });
 
     return res.status(201).json({ token: newUser });
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.get('/', authMiddleware, async (_req, res, next) => {
+  try {
+    const users = await User.findAll();
+
+    return res.status(200).json(users);
   } catch (e) {
     next(e);
   }
