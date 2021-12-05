9e8bf6716ef767a5ef42f45aca872accf17959ef
Igor Marcos Riegel <igor.riegel@gmail.com>
2021-12-05 19:50:09 -0300

    implemmentada a rota de get categories

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+
+const { Category } = require('../models');
 const { categoryServices } = require('../services');
 const authMiddleware = require('../middlewares/authMiddleware');
 
@@ -8,6 +10,16 @@ router.post('/', authMiddleware, async (req, res, next) => {
     const newCategory = await categoryServices.createCategory({ name });
 
     return res.status(201).json(newCategory);
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.get('/', authMiddleware, async (req, res, next) => {
+  try {
+    const categoryList = await Category.findAll();
+
+    return res.status(200).json(categoryList);
   } catch (e) {
     next(e);
   }
