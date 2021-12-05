c127739dbaadd0ffff2888e12d37047b4ef718b4
Igor Marcos Riegel <igor.riegel@gmail.com>
2021-12-05 19:25:39 -0300

    importado controller de category para o index de controllers

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,9 @@
 const userController = require('./userController');
 const loginController = require('./loginController');
+const categoryController = require('./categoryController');
 
 module.exports = {
   userController,
   loginController,
+  categoryController,
 };
