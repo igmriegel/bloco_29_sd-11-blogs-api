f5af19a27828f993efb6c08d9b9010e294aef8f8
Igor Marcos Riegel <igor.riegel@gmail.com>
2021-12-05 23:53:01 -0300

    adicionado o controller de blogPost ao index

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,11 @@
 const userController = require('./userController');
 const loginController = require('./loginController');
 const categoryController = require('./categoryController');
+const postController = require('./postController');
 
 module.exports = {
   userController,
   loginController,
   categoryController,
+  postController,
 };
