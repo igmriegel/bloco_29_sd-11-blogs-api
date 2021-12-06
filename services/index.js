44fe79f2d78ae416053f56df1ddf8bb35fc74e5b
Igor Marcos Riegel <igor.riegel@gmail.com>
2021-12-06 02:33:58 -0300

    adicionado postservices ao index de services

--- services/index.js
+++ services/index.js
@@ -1,9 +1,11 @@
 const userServices = require('./userServices');
 const loginServices = require('./loginServices');
 const categoryServices = require('./categoryServices');
+const postServices = require('./postServices');
 
 module.exports = {
   userServices,
   loginServices,
   categoryServices,
+  postServices,
 };
